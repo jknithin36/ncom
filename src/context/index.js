9d9e66152f40fc513c394699866cded4c4e75032
unknown <jknithin36@gmail.com>
2023-09-03 11:48:37 +0530

    intial commit

---
+++ src/context/index.js
@@ -0,0 +1,14 @@
+"use client";
+
+import { createContext, useState } from "react";
+
+export const GlobalContext = createContext(null);
+
+export default function GlobalState({ children }) {
+  const [showNavModal, setShowNavModal] = useState(false);
+  return (
+    <GlobalContext.Provider value={{ showNavModal, setShowNavModal }}>
+      {children}
+    </GlobalContext.Provider>
+  );
+}
