c26239bea7700fa2d5123845da4fe34528fb05d3
unknown <jknithin36@gmail.com>
2023-09-09 17:34:42 +0530

    p25c

--- src/components/CommonListing/ProductTile/index.js
+++ src/components/CommonListing/ProductTile/index.js
@@ -1,8 +1,12 @@
 "use client";
 
+import { useRouter } from "next/navigation";
+
 export default function ProductTile({ item }) {
+  const router = useRouter();
+
   return (
-    <div>
+    <div onClick={() => router.push(`/product/${item._id}`)}>
       <div className="overflow-hideen aspect-w-1 aspect-h-1 h-52">
         <img
           src={item.imageUrl}
@@ -19,7 +23,20 @@ export default function ProductTile({ item }) {
       ) : null}
       <div className="my-4 mx-auto flex w-10/12 flex-col items-start justify-between">
         <div className="mb-2 flex">
-          <p className={`mr-3 text-sm font-semibold `}>{`$ ${item.price}`}</p>
+          <p
+            className={`mr-3 text-sm font-semibold ${
+              item.onSale === "yes" ? "line-through" : ""
+            }`}
+          >{`$ ${item.price}`}</p>
+          {item.onSale === "yes" ? (
+            <p className="mr-3 text-sm font-semibold text-red-700">{`$ ${(
+              item.price -
+              item.price * (item.priceDrop / 100)
+            ).toFixed(2)}`}</p>
+          ) : null}
+          {item.onSale === "yes" ? (
+            <p className="mr-3 text-sm font-semibold">{`-(${item.priceDrop}%)off`}</p>
+          ) : null}
         </div>
         <h3 className="md-2 text-gray-400 text-sm">{item.name}</h3>
       </div>
--- src/services/product/index.js
+++ src/services/product/index.js
@@ -73,3 +73,39 @@ export const deleteAProduct = async (id) => {
     console.log(e);
   }
 };
+
+export const productByCategory = async (id) => {
+  try {
+    const res = await fetch(
+      `http://localhost:3000/api/admin/product-by-category?id=${id}`,
+      {
+        method: "GET",
+        cache: "no-store",
+      }
+    );
+
+    const data = await res.json();
+
+    return data;
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+export const productById = async (id) => {
+  try {
+    const res = await fetch(
+      `http://localhost:3000/api/admin/product-by-id?id=${id}`,
+      {
+        method: "GET",
+        cache: "no-store",
+      }
+    );
+
+    const data = await res.json();
+
+    return data;
+  } catch (e) {
+    console.log(e);
+  }
+};
